0229f8f085d6b3addbd9b561ef047614f67520d6
Yamini Sharma <yaminis341@gmail.com>
2024-04-24 21:29:27 +0530

    feature: basic setup

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView edges={['top']}>
+      <Text>Radha soami</Text>
+    </SafeAreaView>
+  );
+};
+export default HomeScreen;
