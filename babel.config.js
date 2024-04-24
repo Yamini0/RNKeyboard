c2970ca3ae7e62ab7fc846684935aa7a0ee7dce4
Yamini Sharma <yaminis341@gmail.com>
2024-04-24 21:28:39 +0530

    initial setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+          '@components': './src/components',
+          '@redux': './src/redux',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@typings': './src/typings',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
